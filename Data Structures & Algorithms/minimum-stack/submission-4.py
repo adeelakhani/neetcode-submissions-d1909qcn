159824b421eb1041f97ef261ffa87a79df261de8
Adeel Akhani <akhaniadeel@gmail.com>
2026-03-30 20:40:45 -0400

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,23 @@
+class MinStack:
+    def __init__(self):
+        self.minimum = float('inf')
+        self.stack = []
+        self.minTrack = []
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if val < self.minimum:
+            self.minimum = val
+        self.minTrack.append(self.minimum)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minTrack.pop()
+        if self.minTrack:
+            self.minimum = self.minTrack[-1]
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minTrack[-1]
+        
