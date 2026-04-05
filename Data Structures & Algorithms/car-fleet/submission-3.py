4016b129c3123cb40f1d98457eed1591ca617d5b
Adeel Akhani <akhaniadeel@gmail.com>
2026-04-04 21:00:55 -0400

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = sorted(zip(position, speed)) # o(nlogn) :((((
+        # linear relationship: time to reach target
+        # target = position + speed * time
+        # so isolate for time, we get
+        # time = (target - position)/speed
+        times = []
+        for pos,spd in pairs:
+            times.append((target - pos)/spd)
+        clusters = []
+        stack = []
+        # if a time BEFORE is smaller than a time ahead then its part of that cluster
+        for i in range(0, len(times)):
+            tmp = []
+            while stack and stack[-1] < times[i]:
+                tmp.append(times[stack[-1]])
+                stack.pop()
+            clusters.append(tmp)
+            stack.append(times[i])
+        return len(clusters)-1
+
+
+
+            
+        
+
