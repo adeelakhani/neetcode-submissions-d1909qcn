b14c5088d82154292a3f70111d2322efbbaeb687
Adeel Akhani <akhaniadeel@gmail.com>
2026-07-15 20:22:02 -0400

    Add: string-encode-and-decode - submission-45

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-45.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = []
+        for s in strs:
+            encoded.append(str(len(s))+s)
+        return "".join(encoded)
+    def decode(self, s: str) -> List[str]:
+        res = []
+        c = 0
+        while c<len(s):
+            length = int(s[c])
+            c+=1
+            currIndex = c
+            string = ""
+            while c < len(s) and c < currIndex + length:
+                string += s[c]
+                c+=1
+            res.append(string)
+            print(res)
+        return res
+                
