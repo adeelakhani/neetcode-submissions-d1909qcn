9d2e55164a14be1b5b391abc196a12ee41d43a3b
Adeel Akhani <akhaniadeel@gmail.com>
2026-09-04 00:23:27 -0400

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def recurse(n, seen):
+            if n in (0, 1):
+                return 1
+            elif n < 0:
+                return 0
+            else:
+                seen[n-1] = recurse(n-1, seen)
+                seen[n-2] = recurse(n-2, seen)
+                return seen[n-1] + seen[n-2]
+                
+        seen = {0: 1, 1: 1}
+        return recurse(n, seen)
