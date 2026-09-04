96246add6f4f168dbbfebd84fa8a3479bf068bf1
Adeel Akhani <akhaniadeel@gmail.com>
2026-09-04 00:19:02 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n in (0, 1):
+            return 1
+        elif n < 0:
+            return 0
+        else:
+            return self.climbStairs(n-1) + self.climbStairs(n-2)
